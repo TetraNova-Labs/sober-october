a580ef139a26712c8d4294f66bd64652217c02d2
Albert Mírek <albert.mirek@bonami.cz>
2025-07-22 22:30:15 +0200

    chore(env): update with db env variables

--- api/src/config/validationSchema.ts
+++ api/src/config/validationSchema.ts
@@ -3,6 +3,12 @@ import * as z from "zod"
 export const envSchema = z.object({
     // NODE_ENV: z.enum(['dev', 'prod', 'staging']),
     DB_URL: z.string().url(),
+    DB_HOST: z.string(),
+    DB_PORT: z.string(),
+    DB_USERNAME: z.string(),
+    DB_PASSWORD: z.string(),
+    DB_DATABASE: z.string(),
+
 })
 
 export type EnvSchema = z.infer<typeof envSchema>;
