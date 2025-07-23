c3f3c5fdb5d6bf75cef946f18560be9dc481b064
Albert Mírek <albert.mirek@bonami.cz>
2025-07-23 22:26:22 +0200

    fix: add afterAll to current tests to close them after finishing

--- api/src/app.controller.spec.ts
+++ api/src/app.controller.spec.ts
@@ -5,14 +5,19 @@ import {AppModule} from "./app.module";
 
 describe('AppController', () => {
   let appController: AppController;
+  let app: TestingModule
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    app = await Test.createTestingModule({
       imports: [AppModule]
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
+
+  afterAll(async() => {
+    await app.close();
+  })
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
