82bac0ebb0fd3945ac6fc643447e40b9c21455c2
Albert Mírek <albert.mirek@bonami.cz>
2025-07-23 21:52:33 +0200

    chore: delete unused env

--- api/src/config/configuration.ts
+++ api/src/config/configuration.ts
@@ -3,7 +3,6 @@
 
 export default () => ({
     NODE_ENV: process.env.NODE_ENV || 'dev',
-    DB_URL: process.env.DATABASE_URL,
     DB_HOST: process.env.DB_HOST,
     DB_PORT: process.env.DB_PORT,
     DB_USERNAME: process.env.DB_USERNAME,
