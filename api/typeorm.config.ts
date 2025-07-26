2da99f895cfe14fc14698b066ede379caa3d145c
Albert Mírek <albert.mirek@bonami.cz>
2025-07-26 17:40:18 +0200

    ci(typeorm): setup migrations

---
+++ api/typeorm.config.ts
@@ -0,0 +1,35 @@
+// typeorm.config.ts
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+
+config();
+
+const configService = new ConfigService();
+
+/*
+export default new DataSource({
+    type: 'postgres',
+    url: configService.get<string>('DB_URL'),
+    entities: ['dist/!**!/!*.entity{.ts,.js}'],
+    migrations: ['dist/migrations/!*.js'],
+});
+*/
+
+export default new DataSource({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'sober-october',
+    synchronize: false,
+    dropSchema: false,
+    logging: false,
+    logger: 'file',
+    entities: ['./**/*.entity{.ts,.js}'],
+    migrations: ['src/migrations/**/*.ts'],
+    subscribers: ['src/subscriber/**/*.ts'],
+    migrationsTableName: 'migration_table',
+});
+
