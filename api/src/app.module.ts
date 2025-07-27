e68f0e75cf8842ddd6bee1171f32d2e5300774a4
Albert Mírek <albert.mirek@bonami.cz>
2025-07-27 16:08:03 +0200

    chore: delete non necessary app service and controller

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
 import { envSchema } from './config/validationSchema';
@@ -26,7 +24,7 @@ import { AuthModule } from './auth/auth.module';
     UserModule,
     AuthModule,
   ],
-  controllers: [AppController, SandboxController],
-  providers: [AppService],
+  controllers: [SandboxController],
+  providers: [],
 })
 export class AppModule {}
