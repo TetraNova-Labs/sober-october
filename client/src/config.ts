96bc2891099e348cfccc54847a470b86e1c27f28
Albert Mírek <albert.mirek@bonami.cz>
2025-08-02 22:35:57 +0200

    chore(client): fix cs

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,13 +1,13 @@
 function getRequiredEnv(key: string): string {
-    const value = import.meta.env[key];
-    if (!value || value === "") {
-        throw new Error(`The environment variable ${key} is not set`);
-    }
-    return value;
+  const value = import.meta.env[key];
+  if (!value || value === "") {
+    throw new Error(`The environment variable ${key} is not set`);
+  }
+  return value;
 }
 
 const config = {
-    apiEndpoint: getRequiredEnv("VITE_API_URL")
+  apiEndpoint: getRequiredEnv("VITE_API_URL"),
 };
 
 export default config;
