284eab9f990111b973a815661cc99eb066ca4c39
Albert Mírek <albert.mirek@bonami.cz>
2025-08-03 13:15:10 +0200

    chore: for now dont ensure on env variables, since only one environment exists

--- api/dataSource.ts
+++ api/dataSource.ts
@@ -1,17 +1,17 @@
-import { DataSource } from 'typeorm';
-import * as path from 'path';
-import { User } from './src/user/user.entity';
+import { DataSource } from "typeorm";
+import * as path from "path";
+import { User } from "./src/user/user.entity";
 
 export const AppDataSource = new DataSource({
-  type: 'mysql',
-  host: '127.0.0.1',
+  type: "mysql",
+  host: "127.0.0.1",
   port: 3306,
-  username: 'root',
-  password: 'root',
-  database: 'sober-october-test',
+  username: "root",
+  password: "root",
+  database: "sober-october-test",
   synchronize: false,
   logging: false,
-  entities: [__dirname + '/../**/*.entity.js'],
-  migrations: [path.join(__dirname, 'src', 'migrations', '*.{js,ts}')],
-  migrationsTableName: 'migrations_history',
+  entities: [__dirname + "/../**/*.entity.js"],
+  migrations: [path.join(__dirname, "src", "migrations", "*.{js,ts}")],
+  migrationsTableName: "migrations_history",
 });
