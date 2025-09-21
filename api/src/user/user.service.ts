75f211189523522f8160d46c42d87b16b0a8ac3c
Albert Mírek <albert.mirek@bonami.cz>
2025-09-21 18:17:57 +0200

    feat: add find by user id

--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -13,6 +13,10 @@ export class UserService {
     return this.userRepository.findOneBy({ email });
   }
 
+  findById(id: number): Promise<User | null> {
+    return this.userRepository.findOneBy({ id });
+  }
+
   findAll(): Promise<User[]> {
     return this.userRepository.find();
   }
